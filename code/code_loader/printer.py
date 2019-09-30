8d3168af952742aa58880441a8c4277cde515d80
Andrea F. Daniele <afdaniele@ttic.edu>
2019-09-30 11:45:59 -0500

    code format

--- code/code_loader/printer.py
+++ code/code_loader/printer.py
@@ -5,21 +5,19 @@ from threading import Thread
 
 class CodeLoaderPrinter(Thread):
 
-  def __init__(self, code_loader):
-    Thread.__init__(self)
-    self.code_loader = code_loader
-    self.is_shutdown = False
-    self.frequency = 1.0
+    def __init__(self, code_loader):
+        Thread.__init__(self)
+        self.code_loader = code_loader
+        self.is_shutdown = False
+        self.frequency = 1.0
 
-  def run(self):
-    # TODO: re-enable
-    # while not self.code_loader.is_shutdown():
-    while True:
-      code_loader_status = self.code_loader.get_status()
-      print(json.dumps(code_loader_status, indent=4, sort_keys=True))
-      if self.is_shutdown:
-        break
-      time.sleep(1.0 / self.frequency)
+    def run(self):
+        while not self.code_loader.is_shutdown():
+            code_loader_status = self.code_loader.get_status()
+            print(json.dumps(code_loader_status, indent=4, sort_keys=True))
+            if self.is_shutdown:
+                break
+            time.sleep(1.0 / self.frequency)
 
-  def stop(self):
-    self.is_shutdown = True
+    def stop(self):
+        self.is_shutdown = True
--- code/code_loader/rest_api.py
+++ code/code_loader/rest_api.py
@@ -5,52 +5,53 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 PORT = 8081
 INCLUDE_OUTPUT = False
 
+
 class CodeLoaderRESTAPI(Thread):
 
-  def __init__(self, code_loader):
-    Thread.__init__(self)
-    self.code_loader = code_loader
-    self.is_shutdown = False
+    def __init__(self, code_loader):
+        Thread.__init__(self)
+        self.code_loader = code_loader
+        self.is_shutdown = False
 
-  def run(self):
-    server_address = ('', PORT)
-    httpd = CodeLoaderHTTPServer(server_address, self.code_loader)
-    httpd.serve_forever()
+    def run(self):
+        server_address = ('', PORT)
+        httpd = CodeLoaderHTTPServer(server_address, self.code_loader)
+        httpd.serve_forever()
 
-  def stop(self):
-    self.is_shutdown = True
+    def stop(self):
+        self.is_shutdown = True
 
 
 class CodeLoaderHTTPRequestHandler(BaseHTTPRequestHandler):
 
-  def _set_headers(self):
-    # open headers
-    self.send_response(200)
-    self.send_header('Content-type', 'application/json')
-    # support CORS
-    if 'Origin' in self.headers:
-      self.send_header('Access-Control-Allow-Origin', self.headers['Origin'])
-    # close headers
-    self.end_headers()
+    def _set_headers(self):
+        # open headers
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        # support CORS
+        if 'Origin' in self.headers:
+            self.send_header('Access-Control-Allow-Origin', self.headers['Origin'])
+        # close headers
+        self.end_headers()
 
-  def do_GET(self):
-    self._set_headers()
-    code_loader_status = self.server.code_loader.get_status()
-    if not INCLUDE_OUTPUT:
-      for lvl in code_loader_status['progress']:
-        code_loader_status['progress'][lvl]['output'] = None
-    res = json.dumps(code_loader_status, indent=4, sort_keys=True).encode()
-    self.wfile.write(res)
+    def do_GET(self):
+        self._set_headers()
+        code_loader_status = self.server.code_loader.get_status()
+        if not INCLUDE_OUTPUT:
+            for lvl in code_loader_status['progress']:
+                code_loader_status['progress'][lvl]['output'] = None
+        res = json.dumps(code_loader_status, indent=4, sort_keys=True).encode()
+        self.wfile.write(res)
 
-  def do_HEAD(self):
-    self._set_headers()
+    def do_HEAD(self):
+        self._set_headers()
 
-  def log_message(self, format, *args):
-    return
+    def log_message(self, format, *args):
+        return
 
 
 class CodeLoaderHTTPServer(HTTPServer):
 
-  def __init__(self, server_address, code_loader):
-    HTTPServer.__init__(self, server_address, CodeLoaderHTTPRequestHandler)
-    self.code_loader = code_loader
+    def __init__(self, server_address, code_loader):
+        HTTPServer.__init__(self, server_address, CodeLoaderHTTPRequestHandler)
+        self.code_loader = code_loader
